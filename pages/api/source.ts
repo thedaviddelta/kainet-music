1b31be44a5d47e98477f211634b464e45b3c0d81
David <info@thedaviddelta.com>
2021-06-18 00:11:23 +0200

    Stream error handling

--- pages/api/source.ts
+++ pages/api/source.ts
@@ -9,25 +9,23 @@ const handler: NextApiHandler = async (req, res) => {
     if (!id || Array.isArray(id))
         return res.status(400).json({ error: "Missing ID" });
 
+    const stream = ytdl(id, {
+        quality: "highestaudio"
+    });
+
     res.setHeader("Accept-Ranges", "bytes");
-    ytdl(id, {
-        quality: "highestaudio",
-        highWaterMark: 1024 * 1024 * 1014,
-        dlChunkSize: 1024 * 1024 * 1014
-    }).on(
-        "info",
-        (_, format: videoFormat) => {
-            format.mimeType && res.setHeader("Content-Type", format.mimeType);
-            format.contentLength && res.setHeader("Content-Length", format.contentLength);
-            format.contentLength && res.setHeader("Content-Range", `0-${format.contentLength}/${format.contentLength}`);
-        }
-    ).on(
-        "progress",
-        (chunkLength, downloaded, total) => {
-            res.hasHeader("Content-Length") || res.setHeader("Content-Length", total);
-            res.hasHeader("Content-Range") || res.setHeader("Content-Range", `0-${total}/${total}`);
-        }
-    ).pipe(res);
+    stream.on("info", (_, format: videoFormat) => {
+        format.mimeType && res.setHeader("Content-Type", format.mimeType);
+        format.contentLength && res.setHeader("Content-Length", format.contentLength);
+        format.contentLength && res.setHeader("Content-Range", `0-${format.contentLength}/${format.contentLength}`);
+    });
+    stream.on("progress", (chunkLength, downloaded, total) => {
+        res.hasHeader("Content-Length") || res.setHeader("Content-Length", total);
+        res.hasHeader("Content-Range") || res.setHeader("Content-Range", `0-${total}/${total}`);
+    });
+    stream.on("error", stream.destroy);
+
+    stream.pipe(res);
 };
 
 export default handler;
