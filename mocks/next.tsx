68b7ce0757abfdf96d292cbdeff1b166defa16b7
David <info@thedaviddelta.com>
2021-08-28 17:22:06 +0200

    Update dependencies

--- mocks/next.tsx
+++ mocks/next.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { RouterContext } from "next/dist/next-server/lib/router-context";
+import { RouterContext } from "next/dist/shared/lib/router-context";
 
 export const routerMock = {
     basePath: "",
